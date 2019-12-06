cf32688d7388cb7194749b121f52665bacc640bd
Stuart Hsu <yopahsu@gmail.com>
2019-12-06 17:02:08 +0800

    admin data transmission v2.0

--- Server/Express.js
+++ Server/Express.js
@@ -15,50 +15,5 @@ app.get('/', (req, res) => {
   res.send('HEY!');
 });
 
-// app.get('/admin/product.html', (req, res) => {
-//   // res.redirect('/admin/product.html');
-//   res.render('/admin/product');
-// });
-
-// app.post('/admin/product.html', (req, res) => {
-//   const id = req.body.id;
-//   const title = req.body.title;
-//   const s_title = String(title);
-//   const description = req.body.description;
-//   const price = req.body.price;
-//   const texture = req.body.texture;
-//   const wash = req.body.wash;
-//   const place = req.body.place;
-//   const note = req.body.note;
-//   const story = req.body.story;
-//   const color = req.body.color;
-//   const size = req.body.size;
-//   const stock = req.body.stock;
-//
-//   // let inserSQL = `INSERT INTO product (id, title, description, price, texture, wash, place, note, story)
-//   //                  VALUES (${id}, ${title}, ${description}, ${price}, ${texture}, ${wash}, ${place}, ${note}, ${story});`;
-//
-//   let inserSQL = `INSERT INTO product (id, title, description, price, type) VALUES (${id}, '${title}', '${description}', ${price}, "ok");`;
-//
-//                   // let inserSQL = "INSERT INTO product (id, title, description, price, type) VALUES (560, '好', '沒問題', 5, 'ok');";
-//
-//   insertDB = db.query(inserSQL, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//   });
-//
-//   res.redirect('/admin/product.html');
-//   // res.send('OK');
-// });
-
-// app.get('/admin/campaign.html', (req, res) => {
-//   res.send('campaign!');
-// });
-//
-// app.get('/admin/checkout.html', (req, res) => {
-//   res.send('checkout!');
-// });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- Server/routes/adminproduct.js
+++ Server/routes/adminproduct.js
@@ -2,6 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../models/connection_db');
 const router = express.Router(); // create a router
+const multer  = require('multer'); // file upload
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    var newFilename = Math.floor((Math.random() * 1000000));
+    cb(null, newFilename + '.png');
+  }
+});
+var upload = multer({ storage: storage });
+
+// const upload = multer({ dest: 'uploads/' }); // file upload
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,7 +26,7 @@ router.get('/', (req, res) => {
   res.render('/');
 });
 
-router.post('/', (req, res) => {
+router.post('/', upload.array('images', 3), (req, res) => {
   const id = req.body.id;
   const title = req.body.title;
   const description = req.body.description;
@@ -26,17 +40,19 @@ router.post('/', (req, res) => {
   const hot_title = req.body.hot_title;
 
   const color = req.body.color;
+  const imagePath = req.files.map(file => `http://localhost:3000/uploads/${file.filename}`);
 
   const size = req.body.size;
   const stock = req.body.stock;
 
-  let inserProductTB = `INSERT INTO product (id, title, description, price, texture, wash, place, note, story, type, hot_title)
-                  VALUES (${id}, '${title}', '${description}', ${price}, '${texture}', '${wash}', '${place}', '${note}', '${story}', '${type}', '${hot_title}');`;
+  let inserProductTB = `INSERT INTO product (id, title, description, price, texture, wash, place, note, story, type, hot_title, main_image, images)
+                  VALUES (${id}, '${title}', '${description}', ${price}, '${texture}', '${wash}', '${place}', '${note}', '${story}', '${type}', '${hot_title}', '${imagePath[0]}', '${imagePath}');`;
 
   let inserColorTB = `INSERT INTO color (p_id, name, code) VALUES (${id}, '${color}', 'color code');`;
-  // image color_code
-  let inserVariantTB = `INSERT INTO variant (size, stock, s_id, color_code) VALUES ('${size}', ${stock}, '1', 'color code');`;
-  // s_id color_code
+  // color_code
+  const number = Math.random()*10;
+  let inserVariantTB = `INSERT INTO variant (size, stock, s_id, color_code) VALUES ('${size}', ${stock}, ${number}, 'color code');`;
+
 
   db.query(inserProductTB, (err, result) => {
     if (err) {
