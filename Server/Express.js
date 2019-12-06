71c3033ac2e943dcbfb7107d301543f29c78c346
Stuart Hsu <yopahsu@gmail.com>
2019-12-06 09:33:17 +0800

    remove unuse file

--- Server/Express.js
+++ Server/Express.js
@@ -6,4 +6,16 @@ app.get('/', (req, res) => {
   res.send('HEY!');
 });
 
+app.get('/admin/product.html', (req, res) => {
+  res.send('product!');
+});
+
+app.get('/admin/campaign.html', (req, res) => {
+  res.send('campaign!');
+});
+
+app.get('/admin/checkout.html', (req, res) => {
+  res.send('checkout!');
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
