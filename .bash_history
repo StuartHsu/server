e202a336ec12e1114c06b4ec10b60028ba87111f
EC2 Default User <ec2-user@ip-172-31-18-139.us-east-2.compute.internal>
2019-12-06 01:27:04 +0000

    sql db

--- .bash_history
+++ .bash_history
@@ -98,3 +98,57 @@ service mysqld start
 sudo service mysqld start
 mysqladmin -u root password katosy0322
 mysql -uroot -p
+pm2 list
+service mysqld start
+mysql -uroot -p
+mysqldump -u root -p stylish product > product.sql
+ls
+git push https://github.com/StuartHsu/awsserver.git master
+git init
+ls
+scp -i ~/.ssh/katosyyopa.pem ec2-user@ec2–3–135–51–44.us-east-2.compute.amazonaws.com:/Users/stuart/Desktop
+scp -i ~/.ssh/katosyyopa.pem ec2-user@ec2–3–135–51–44.us-east-2.compute.amazonaws.com:/Users/stuart/Desktop .
+git init
+cd .git/
+ls
+cd ..
+git status
+git add .
+git commit -m "server setup"
+git push https://github.com/StuartHsu/awsserver.git master
+git remote add origin https://github.com/StuartHsu/server.git
+git push -u origin master
+mysql -u root -p
+mysql -u root -p stylish product > product.sql
+ls
+git status
+git add .
+git push -u origin master
+git status
+git commit -m "dump sql"
+git push -u origin master
+mysqldump -u root -p stylish product > product.sql
+git add .
+git commit -m "sql"
+git push -u origin master
+mysql -u root -p
+mysqldump -u root -p stylish product > product.sql
+git add .
+git commit -m "sql2"
+git push -u origin master
+git clone https://github.com/StuartHsu/server.git
+ls
+pm2 list
+pm2 stop 0
+ls
+cd server/
+ls
+cd Server/
+ls
+pwd
+cd ~
+pm2 start /home/ec2-user/server/Server/Epress.js
+pm2 start /home/ec2-user/server/Server/Express.js
+pm2 stop 1
+pm2 restart 1
+mysql -u root -p
--- product.sql
+++ product.sql
@@ -23,8 +23,18 @@ DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `product` (
-  `id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `title` varchar(255) NOT NULL,
+  `id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'Product id.',
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT 'Product title.',
+  `description` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT 'Product description.',
+  `price` bigint(20) unsigned NOT NULL COMMENT 'Product price.',
+  `texture` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Product texture.',
+  `wash` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'The way we can wash the product.',
+  `place` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Place of production.',
+  `note` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'The note of product.',
+  `story` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Product multiline story.',
+  `main_image` varchar(255) DEFAULT NULL COMMENT 'Main image.',
+  `hot_title` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Title of the hots section.',
+  `type` varchar(50) NOT NULL COMMENT 'Product type.',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -35,7 +45,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'test'),(2,'Stuart');
+INSERT INTO `product` VALUES (1,'test','',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(2,'Stuart','',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(3,'Hey','',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(4,'?','',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(5,'','我',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(6,'','我',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(7,'','hey',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,''),(1234,'????????','???????????????',2200,'??????','?????40?)','??','??????????','????????????','https://stylish.com/main.jpg','???????',''),(1235,'????????','???????????????',2200,'??????','?????40?)','??','??????????','????????????','https://stylish.com/main.jpg','???????',''),(1236,'厚實毛呢格子外套','高抗寒素材選用，保暖也時尚有型',2200,'棉、聚脂纖維','手洗（水溫40度)','韓國','實品顏色以單品照為主','你絕對不能錯過的超值商品','https://stylish.com/main.jpg','冬季新品搶先看','');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-05  5:16:02
+-- Dump completed on 2019-12-06  1:20:51
